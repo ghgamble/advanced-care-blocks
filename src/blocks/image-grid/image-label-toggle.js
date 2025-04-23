81ff137c52b061f4c0026694ebf31c874f495541
Grace Gamble <gracegamble@Graces-Laptop.local>
2025-04-23 11:37:13 -0600

    adjust image grid styling for touch screens

--- src/blocks/image-grid/image-label-toggle.js
+++ src/blocks/image-grid/image-label-toggle.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    if (window.innerWidth <= 768) {
-      document.querySelectorAll('[data-touchable]').forEach((figure) => {
-        figure.addEventListener('click', () => {
-          figure.classList.toggle('touched');
-        });
+  if (window.matchMedia('(hover: none) and (pointer: coarse)').matches) {
+    document.querySelectorAll('[data-touchable]').forEach((figure) => {
+      figure.addEventListener('click', () => {
+        figure.classList.toggle('touched');
       });
-    }
+    });
+  }
 });
-  
