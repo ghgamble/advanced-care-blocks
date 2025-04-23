47dfbdd5c874e708407db75583d7c59f87792bfc
Grace Gamble <gracegamble@Graces-Laptop.local>
2025-04-23 14:21:35 -0600

    adjust swash background

--- advanced-care-blocks.php
+++ advanced-care-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Advanced Care Blocks
  * Description: Custom Gutenberg blocks for the Advanced Care website.
- * Version: 1.0.12
+ * Version: 1.0.13
  * Author: GG Dev
  * Author URI: https://gg-dev.co/
  * GitHub Plugin URI: https://github.com/ghgamble/advanced-care-blocks
