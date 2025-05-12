1131adb04782feac4c913be993dbc887eb9da78b
Grace Gamble <gracegamble@Graces-Laptop.local>
2025-05-12 13:20:07 -0600

    slightly adjust css for styled image

--- advanced-care-blocks.php
+++ advanced-care-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Advanced Care Blocks
  * Description: Custom Gutenberg blocks for the Advanced Care website.
- * Version: 2.0.0
+ * Version: 2.0.1
  * Author: GG Dev
  * Author URI: https://gg-dev.co/
  * GitHub Plugin URI: https://github.com/ghgamble/advanced-care-blocks
